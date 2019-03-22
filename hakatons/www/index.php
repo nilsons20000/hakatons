6f1577387c532ca3ec541359e06a52a32538e876
sloppynick3 <sloppynick3@gmail.com>
2019-03-22 14:55:21 +0200

    Nomainiju pagination

--- hakatons/www/index.php
+++ hakatons/www/index.php
@@ -17,22 +17,34 @@ include "functions/functions.php";
 <!DOCTYPE html>
 <html lang="lv" xmlns="http://www.w3.org/1999/html">
 <head>
-    <title>Atrodi Skolu</title>
-    <script type="text/javascript" src="./js/jquery.js"></script>
-    <script type="text/javascript" src="./js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="./js/menu.js"></script>
-    <script src="https://use.fontawesome.com/320ac68418.js"></script>
-    <script type='text/javascript' src='http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js'></script>
-    <script type="text/javascript" src="./js/jquery.simplePagination.js"></script>
-    <script type="text/javascript" src="./js/updatePaginator.js"></script>
+
+    <title></title>
+    <script src="./js/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <link rel="stylesheet" type="text/css" href="css/tabula.css">
     <link rel="stylesheet" type="text/css" href="css/filter.css">
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.css">
-    <link rel="stylesheet" type="text/css" href="./css/simplePagination.css">
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.css"/>
+
+    <script src="https://use.fontawesome.com/320ac68418.js"></script>
+    <script type='text/javascript' src='http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js'></script>
 
 
+    <script src="./js/bootstrap.min.js"></script>
+
+
+    <script>
+        $(document).ready(function () {
+            $("#myInput").on("keyup", function () {
+                var value = $(this).val().toLowerCase();
+                $("#myTable tr:not(table_row)").filter(function () {
+                    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+                });
+            });
+        });
+    </script>
+    <script type="text/javascript" src="./js/menu.js"></script>
 </head>
 <body>
 <div id="page-wrap">
@@ -46,22 +58,12 @@ include "functions/functions.php";
                onclick="closeNav()">×</a>
             <div class="overlay-content">
                 <?php
-                require 'views/form.view.php';
+                printForm($database);
                 ?>
             </div>
         </div>
 
         <span id="open" onclick="openNav()">☰ open</span>
-        <script>
-            $(document).ready(function () {
-                $("#myInput").on("keyup", function () {
-                    var value = $(this).val().toLowerCase();
-                    $("#myTable tr").filter(function () {
-                        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-                    });
-                });
-            });
-        </script>
     </header>
     <div id="map" style="width: 100%; height: 440px; border: 1px solid #AAA;"></div>
     <script>
@@ -81,16 +83,18 @@ include "functions/functions.php";
         }
     </script>
     <nav class="dws-menu">
-        <input id="myInput" type="text" placeholder="Search..">
-        <div id="pagination" class="light-theme simple-pagination">
-            <ul>
-                <li><a href="#page-1" class="page-link prev">Prev</a></li>
-                <li><a href="#page-1" class="page-link">1</a></li>
-                <li class="active"><span class="current">2</span></li>
-                <li><a href="#page-3" class="page-link">3</a></li>
-                <li><a href="#page-3" class="page-link next">Next</a></li>
-            </ul>
+        <div class="form-group">
+            <select name="state" id="maxRows" class="form-control" style="width:150px;">
+                <option value="5000">Rādīt visus</option>
+                <option value="10">5</option>
+                <option value="20">10</option>
+                <option value="50">25</option>
+                <option value="100">50</option>
+                <option value="150">75</option>
+                <option value="200">100</option>
+            </select>
         </div>
+        <input id="myInput" type="text" placeholder="Search..">
         <table width="100%" id="table">
             <thead>
             <tr>
@@ -103,14 +107,73 @@ include "functions/functions.php";
             if ($schools != null) {
                 require "views/table.view.php";
             };
-
             ?>
+            <script>
+                // Basic example
+                $(document).ready(function () {
+                    $('#dtBasicExample').DataTable({
+                        "paging": false
+                    });
+                    $('.dataTables_length').addClass('bs-select');
+                });
+
+                $(document).ready(function () {
+                    $('#dtBasicExample').DataTable({
+                        "pagingType": "simple"
+                    });
+                    $('.dataTables_length').addClass('bs-select');
+                });
+            </script>
             </tbody>
         </table>
-        <script>
-            updateTable();
-        </script>
+        <div class="pagination-container">
+            <nav>
+                <ul class="pagination"></ul>
+            </nav>
+        </div>
     </nav>
 </div>
+
+
+<script>
+    var table = '#table';
+    $('#maxRows').on('change', function () {
+        $('.pagination').html('');
+        var trnum = 0;
+        var maxRows = parseInt($(this).val());
+        var totalRows = $(table + ' tbody tr').length;
+        $(table + ' tr:gt(0)').each(function () {
+            trnum++;
+            if (trnum > maxRows) {
+                $(this).hide()
+            }
+            if (trnum <= maxRows) {
+                $(this).show()
+            }
+        });
+        if (totalRows > maxRows) {
+            var pagenum = Math.ceil(totalRows / maxRows);
+            for (var i = 1; i <= pagenum;) {
+                $('.pagination').append('<li data-page="' + i + '">\<span>' + i++ + '<span class="sr-only">(current)</span></span>\</li>').show()
+            }
+        }
+        $('.pagination li:first-child').addClass('active');
+        $('.pagination li').on('click', function () {
+            var pageNum = $(this).attr('data-page');
+            var trIndex = 0;
+            $('.pagination li').removeClass('active');
+            $(this).addClass('active');
+            $(table + ' tr:gt(0)').each(function () {
+                trIndex++;
+                if (trIndex > (maxRows * pageNum) || trIndex <= ((maxRows * pageNum) - maxRows)) {
+                    $(this).hide()
+                } else {
+                    $(this).show()
+                }
+            })
+        })
+    })
+
+</script>
 </body>
 </html>
