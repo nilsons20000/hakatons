31b665e4b6b953e18d8e80ee8c693af4d8e38c4d
sloppynick3 <sloppynick3@gmail.com>
2019-03-28 17:48:11 +0200

    ORDER BY ielikts

--- hakatons/www/functions/filter.php
+++ hakatons/www/functions/filter.php
@@ -70,7 +70,7 @@ function filter($posts,$database){
 		FROM
 			izglitibas_profesijas
 		LEFT JOIN profesijas ON izglitibas_profesijas.profesija = profesijas.ID) AS profesijas ON macibu_iestades.ID = profesijas.iestades_ID 
-        LEFT JOIN macibu_iestades_papildus ON macibu_iestades.ID = macibu_iestades_papildus.iestades_ID '.$queryWhere.' group by macibu_iestades.ID limit 100;';
+        LEFT JOIN macibu_iestades_papildus ON macibu_iestades.ID = macibu_iestades_papildus.iestades_ID '.$queryWhere.' group by macibu_iestades.ID limit 100 ORDER BY macibu_iestades.nosaukums ASC;';
 
     return $database->getFiltered($queryDefault);
 }
